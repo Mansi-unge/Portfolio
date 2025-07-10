58e9032068ed876fcb0299b6a7865a1c0fd31aae
MANSI UNGE <mansiunge@gmail.com>
2025-07-10 15:07:13 +0530

    Update BlogPage.jsx

--- src/Pages/BlogPage.jsx
+++ src/Pages/BlogPage.jsx
@@ -19,7 +19,7 @@ const dummyBlogs = [
   date: "July 9, 2025",
   coverImage:
     "https://cdn.hashnode.com/res/hashnode/image/upload/v1752054014089/56b7a7e6-d5d5-4426-942b-cf65068198ec.png?w=1600&h=840&fit=crop&crop=entropy&auto=compress,format&format=webp", 
-  url: "https://mansiunge.hashnode.dev/from-changing-button-colors-to-building-full-stack-dreams-my-mern-journey-so-far"
+  url: "/blog.png"
 }
 ];
 
