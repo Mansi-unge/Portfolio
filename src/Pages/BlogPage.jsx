5e5e913f498d20b997255b9df075f67de4d65211
MANSI UNGE <mansiunge@gmail.com>
2025-07-10 15:22:34 +0530

    Update BlogPage.jsx

--- src/Pages/BlogPage.jsx
+++ src/Pages/BlogPage.jsx
@@ -18,8 +18,8 @@ const dummyBlogs = [
     "How I went from fearing the backend to embracing full-stack development with the MERN stack — the ups, the bugs, and the breakthroughs along the way.",
   date: "July 9, 2025",
   coverImage:
-    "https://cdn.hashnode.com/res/hashnode/image/upload/v1752054014089/56b7a7e6-d5d5-4426-942b-cf65068198ec.png?w=1600&h=840&fit=crop&crop=entropy&auto=compress,format&format=webp", 
-  url: "https://cdn.hashnode.com/res/hashnode/image/upload/v1752043085256/9e06a281-ae2f-40d1-acfa-d213dcaad554.jpeg?w=1600&h=840&fit=crop&crop=entropy&auto=compress,format&format=webp"
+    "https://cdn.hashnode.com/res/hashnode/image/upload/v1752043085256/9e06a281-ae2f-40d1-acfa-d213dcaad554.jpeg?w=1600&h=840&fit=crop&crop=entropy&auto=compress,format&format=webp", 
+  url: "https://mansiunge.hashnode.dev/from-changing-button-colors-to-building-full-stack-dreams-my-mern-journey-so-far"
 }
 ];
 
