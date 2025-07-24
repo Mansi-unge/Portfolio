3914dedaa60fa80edb3673f3709fadf3aed599dc
MANSI UNGE <mansiunge@gmail.com>
2025-07-24 23:29:27 +0530

    Update BlogPage.jsx

--- src/Pages/BlogPage.jsx
+++ src/Pages/BlogPage.jsx
@@ -90,7 +90,7 @@ const BlogPage = () => {
       </motion.div>
 
       {/* Blog Cards Grid */}
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-10 pb-10">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10 pb-10">
         {blogs.length > 0 ? (
           blogs.map((blog, index) => <BlogCard key={index} blog={blog} />)
         ) : (
